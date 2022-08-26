97ccb22dda07b0b644c36c3a2c87e21cacf5a94f
Sebastian Rafal Smal <sebastian.smal@gmx.de>
2022-08-26 12:16:08 +0200

    Initial Commit

---
+++ lottoBeEasy/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  lottoBeEasy
+//
+//  Created by Rafael on 26.08.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
